17c82849ab6c64cb9208f54ad29dadb72b406707
Ansh Gujral <anshgujral1997@gmail.com>
2018-10-06 19:16:40 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center', justifyContent: 'center', height: 100}}>
+        <Text>Hello</Text>
+      </View>
+    )
+  }
+}
