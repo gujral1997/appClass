be6d76bc563344b99b62cfad37831e67f5651015
Ansh Gujral <anshgujral1997@gmail.com>
2018-10-16 18:16:34 +0530

    taught view(basic styling)

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Dimensions } from 'react-native'
+
+const { width, height } = Dimensions.get('window')
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={{alignItems: 'center', justifyContent: 'center', height: 100}}>
-        <Text>Hello</Text>
+      <View style={{alignItems: 'center', justifyContent: 'center', height: height, backgroundColor: 'red'}}>
+        <View style={{backgroundColor: 'blue',alignItems: 'center', justifyContent: 'center',flex: 1, width:width}}/>
+        <View style={{backgroundColor: 'yellow',flex: 0.1, width:width}}/>
       </View>
     )
   }
